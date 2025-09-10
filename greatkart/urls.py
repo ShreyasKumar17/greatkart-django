4d53aeb3996cb519fcd0ec648768fe8e3c50dc27
ShreyasKumar17 <shreyaskumar360@gmail.com>
2025-09-11 02:24:35 +0530

    store and carts functionality

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -19,10 +19,12 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 
     # placeholder routes
     path('signin/', views.signin, name='signin'),
