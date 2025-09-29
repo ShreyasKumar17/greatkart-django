9438a9566caa075eb2cac1240acbad0968b07439
ShreyasKumar17 <shreyaskumar360@gmail.com>
2025-09-30 00:07:52 +0530

    reviews and rating system

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Variation
+from .models import Product, Variation, ReviewRating
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
@@ -14,5 +14,13 @@ class VariationAdmin(admin.ModelAdmin):
     list_editable = ('is_active',)
     list_filter = ('product', 'variation_category', 'variation_value')
 
+class ReviewRatingAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'subject','review', 'rating', 'status', 'created_at')
+    list_editable = ('status',)
+    list_filter = ('status', 'created_at', 'rating')
+    search_fields = ('subject', 'review', 'user__email', 'product__product_name')
+
+
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Variation,VariationAdmin)
+admin.site.register(ReviewRating,ReviewRatingAdmin)
--- store/views.py
+++ store/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import redirect, render, get_object_or_404
 from carts.models import CartItem
 from category.models import Category
-from .models import Product
+from orders.models import OrderProduct
+from .models import Product, ReviewRating
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator  
 from django.db.models import Q  
+from .forms import ReviewForm
 
 # Create your views here.
 
@@ -37,13 +40,27 @@ def product_detail(request, category_slug, product_slug):
         single_product = Product.objects.get(category__Slug=category_slug, slug=product_slug)
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
         
-    
     except Exception as e:
         raise e
+    
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user = request.user, product_id = single_product.id).exists() # type: ignore
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    
+    else:
+        orderproduct = None
+
+    #Get the reviews
+    reviews = ReviewRating.objects.filter(product=single_product, status=True)
+
 
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
+        'orderproduct': orderproduct,
+        'reviews': reviews,
     }
     
     return render(request, 'store/product_detail.html',context)
@@ -61,3 +78,36 @@ def search(request):
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
+
+
+
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER', '/')
+    product = get_object_or_404(Product, id=product_id)
+
+    if request.method == 'POST':
+        try:
+            # Try to get an existing review
+            review_instance = ReviewRating.objects.get(user=request.user, product=product)
+        except ReviewRating.DoesNotExist:
+            # Create a new empty review object
+            review_instance = ReviewRating()
+
+        form = ReviewForm(request.POST, instance=review_instance)
+
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.product = product
+            review.ip = request.META.get('REMOTE_ADDR')
+            review.save()
+
+            if review_instance.id: # type: ignore
+                messages.success(request, "Thank you! Your review has been updated.")
+            else:
+                messages.success(request, "Thank you! Your review has been submitted.")
+        else:
+            messages.error(request, f"Error: {form.errors}")
+
+        return redirect(url)
